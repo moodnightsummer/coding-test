8ad784c063cffeeed5ecd216809c7308de71edc5
jelly <moodnightsummer@naver.com>
2022-11-30 23:27:25 +0900

    commit

---
+++ programmers/chapter_2/minCommonValue.js
@@ -0,0 +1,22 @@
+function gcd(a, b) {
+  while (b > 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function solution(arr) {
+  let answer = 1;
+  for (let i = 0; i < arr.length; i++) {
+    answer = lcm(answer, arr[i]);
+  }
+  return answer;
+}
+
+console.log(solution([1, 2, 3]));
